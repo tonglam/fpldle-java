c2dd186c504995c0a63c864a5a87d5899d3f0e0b
tong <bluedragon00000@gmail.com>
2022-02-19 00:37:18 +0800

    init

--- src/main/java/com/tong/fpl/util/HttpUtils.java
+++ src/main/java/com/tong/fpl/util/HttpUtils.java
@@ -14,7 +14,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 import java.io.InputStream;
